c1f6d178e00e9fbc0ac62f166e694173004e22af
Zachary Sain <zachsain@gmail.com>
2022-10-22 14:30:19 -0500

    adding activities to user profile

--- client/src/UserProfile.js
+++ client/src/UserProfile.js
@@ -6,11 +6,14 @@ function UserProfile({user}){
     
 
     return(
-        <div>
-         <h3 className='user'>{user.username}</h3>
-        <img id="user-image" style={{ width: "12%", height: "10%" }} src={user.image_url} alt="user image"/>
-        <p>{user.bio}</p>
-        <p>contact: {user.email}</p>
+        <div className="profile-container">
+            <h3 className='user'>{user.username}</h3>
+            <img id="user-image" style={{ width: "12%", height: "10%" }} src={user.image_url} alt="user image"/>
+            <p>bio:{user.bio}</p>
+            <p>contact: {user.email}</p>
+        </div>, 
+        <div className="user-profile-activites">
+
         </div>
     )
 }
