3359a4d18a3a4190c3ea7c68fff7b932cccb27c3
Zachary Sain <zachsain@gmail.com>
2022-10-22 15:32:24 -0500

    trying to figure out how to filter routines to show up in activities

--- client/src/ActivitiesCard.js
+++ client/src/ActivitiesCard.js
@@ -6,10 +6,23 @@ function ActivitiesCard({
     duration, 
     description,
     likes,
-    routine_id
+    routine_id,
+    user
 }){
+    let routine = user.routines.filter(r=> r.id == routine_id)
 
     return(
-        <div>{title}</div>
+        <div>
+        <h2>title: {title}</h2>
+        <p>category: {category}</p>
+        <p>duration: {duration}</p>
+        <p>notes: {description}</p>
+        <p>routine: {routine.title} </p>
+        {/* <button>likes: {likes}</button> */}
+
+
+        </div>
     )
 }
+
+export default ActivitiesCard
--- client/src/UserActivities.js
+++ client/src/UserActivities.js
@@ -1,11 +1,14 @@
 import React, {useState} from 'react'
+import ActivitiesCard from './ActivitiesCard'
 
 function UserActivities({user}) {
 
     
     let userActivities = user.activities.map(a => {
         return <ActivitiesCard
+                  user={user}
                   key={a.id} 
+                  title={a.title}
                   category={a.category}
                   duration={a.duration}
                   description={a.description}
@@ -18,7 +21,7 @@ function UserActivities({user}) {
     return(
         <div>
             activities here
-            <p>{user.activities}</p>
+            <p>{userActivities}</p>
         </div>
     )
 
